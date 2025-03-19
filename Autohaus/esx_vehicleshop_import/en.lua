b44c575eba872f0572d0aaa3cfe689010a415020
MaxiFaxiPaxi <115405418+maxifaxipaxi-new@users.noreply.github.com>
2025-03-19 10:40:41 +0100

    Create en.lua

---
+++ Autohaus/esx_vehicleshop_import/en.lua
@@ -0,0 +1,18 @@
+Locales['en'] = {
+    ['vehicle_purchased']   = '~g~Fahrzeugkauf erfolgreich!',
+	['vehicle_belongs']   = '~g~Fahrzeugkauf erfolgreich:\n~s~Das Fahrzug mit dem Kennzeichen ~p~%s~s~ gehört jetzt dir.',
+    ['not_enough_money'] = '~r~Du hast nicht genug Geld.',
+    ['shop_menu'] = 'Drücke ~g~[E]~w~ um den Fahrzeugkatalog zu betrachten',
+    ['sell_vehicle'] = 'Drücke ~g~[E]~w~ um dein Auto für ~g~%s~s~ $ zu verkaufen',
+    ['vehicle_sold_for'] = '~g~Fahrzeugverkauf erfolgreich:~s~\n%s für ~g~%s$~s~ verkauft',
+    ['not_yours'] = '~r~Dir gehört dieses Fahrzeug nicht...',
+    ['watch_catalog'] = '~g~[E]~w~ Fahrzeugkatalog',
+    ['wait_vehicle'] = 'Dein Fahrzug wird geladen, gedulde dich einen Moment!',
+
+      -- Test Drive
+    ['testdrive_notification'] = 'Du hast ~g~%s~w~ Sekunden Probefahrt!',
+    ['testdrive_timer'] = 'Probefahrt endet in: %s Sekunden!',
+    ['testdrive_finished'] = 'Probefahrt wurde beendet. Wir hoffen Dir gefällt der Wagen!'
+
+  } --~INPUT_PICKUP~
+  
